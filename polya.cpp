4f0a7809f1ec259e3d77a230311cb553612950d9
cuisonghui <cuisonghui@qq.com>
2016-07-27 12:30:29 +0800

    polya.cpp

---
+++ polya.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stream>
+#include <set>
+
+int polya()
+{
+	unsigned char polyaNum=1;
+	set<int> set_dedup;
+	for(int polyaNum=0;polyaNum<256;polyaNum++)
+	{
+		
+	}
+}
+
+int main()
+{
+
+
+}
