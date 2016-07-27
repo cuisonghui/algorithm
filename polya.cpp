20ac140bfb285fd587f658eb915eaf95820b218b
cuisonghui <cuisonghui@qq.com>
2016-07-27 14:42:35 +0800

    polya

--- polya.cpp
+++ polya.cpp
@@ -1,19 +1,33 @@
-#include <stdio.h>
-#include <stream>
-#include <set>
+#include <iostream>
+#include <map>
+
+
+using namespace std;
+
+void zuoyi(unsigned char& polya)
+{
+	const unsigned char maxv = 0X80;
+	bool maxBit = polya & maxv;
+	polya <<= 1;
+	if(maxBit)
+		polya +=1;
+}
 
 int polya()
 {
 	unsigned char polyaNum=1;
-	set<int> set_dedup;
-	for(int polyaNum=0;polyaNum<256;polyaNum++)
+	map<int,int> map_dedup;
+	for(unsigned char polyaNum=0;polyaNum<256;polyaNum++)
 	{
-		
+		unsigned char value = polyaNum;
+		zuoyi(value);	
+		std::cout <<(int)polyaNum <<"--->" <<(int)(value) << std::endl;
+		if(polyaNum ==255)
+			break;
 	}
 }
 
 int main()
 {
-
-
+	polya();
 }
